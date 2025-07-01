3ee5825e24892d66544de47af32cb3070974d2ea
irpanzy <irfanoputra27@gmail.com>
2025-07-01 14:57:19 +0700

    feat: add cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const routes = require("./routes");
+const cors = require("cors");
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 app.use("/api", routes);
 
