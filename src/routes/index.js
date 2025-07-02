f20a3f94bed11fb645d0f716c22b60032201c3e3
irpanzy <irfanoputra27@gmail.com>
2025-07-02 12:55:23 +0700

    feat: add route for booking

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const authRoute = require("./auth.route");
 const userRoute = require("./user.route");
+const bookingRoute = require("./booking.route");
 
 const router = express.Router();
 
 router.use("/auth", authRoute);
 router.use("/users", userRoute);
+router.use("/bookings", bookingRoute);
 
 module.exports = router;
