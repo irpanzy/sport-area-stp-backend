dbc23a529da0ee106a94e4aab4c72b84d105efa1
irpanzy <irfanoputra27@gmail.com>
2025-07-02 15:10:30 +0700

    update: time slot validation and enhance booking validation logic

--- src/middlewares/booking.middleware.js
+++ src/middlewares/booking.middleware.js
@@ -35,12 +35,16 @@ export const validateCreateBooking = async (req, res, next) => {
 
   // Validasi time_slot
   const validTimeSlots = [
-    "08:00-10:00",
-    "10:00-12:00",
-    "12:00-14:00",
-    "14:00-16:00",
-    "16:00-18:00",
-    "18:00-20:00",
+    "09:00-10:00",
+    "10:00-11:00",
+    "11:00-12:00",
+    "13:00-14:00",
+    "15:00-16:00",
+    "16:00-17:00",
+    "17:00-18:00",
+    "18:00-19:00",
+    "19:00-20:00",
+    "20:00-21:00",
   ];
 
   if (!time_slot || !validTimeSlots.includes(time_slot)) {
@@ -192,12 +196,14 @@ export const validateUpdateBooking = async (req, res, next) => {
 
     if (time_slot !== undefined) {
       const validTimeSlots = [
-        "08:00-10:00",
-        "10:00-12:00",
-        "12:00-14:00",
-        "14:00-16:00",
-        "16:00-18:00",
-        "18:00-20:00",
+        "09:00-10:00",
+        "10:00-11:00",
+        "11:00-12:00",
+        "13:00-14:00",
+        "15:00-16:00",
+        "16:00-17:00",
+        "17:00-18:00",
+        "18:00-19:00",
       ];
 
       if (!validTimeSlots.includes(time_slot)) {
